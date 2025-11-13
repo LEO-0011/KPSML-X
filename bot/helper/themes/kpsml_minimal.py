8b84d08bfbbdf8a3d438ffa6a34bdb089cd56ec6
LEO-0011 <leoyagami123123@gmail.com>
2025-11-13 12:57:25 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -2,10 +2,10 @@
 class KPSMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = '🤖 Repo'
-    ST_BN1_URL = 'https://github.com/Tamilupdates/KPSML-X'
-    ST_BN2_NAME = 'Updates 🔥'
-    ST_BN2_URL = 'https://telegram.me/KPSBots'
+    ST_BN1_NAME = '𝗨ᴩᴅᴀᴛᴇ 𝗖ʜᴀɴɴᴇʟ⚡'
+    ST_BN1_URL = 'https://t.me/Animes_x_Cruise'
+    ST_BN2_NAME = '𝗗ᴇᴠᴇʟᴏᴩᴇʀ‼️'
+    ST_BN2_URL = 'http://t.me/Leoyagamihere'
     ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
 Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
